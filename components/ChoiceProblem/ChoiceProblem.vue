6a4e2d03f93b2c4bad95f033739c897d1334a269
Egor Dubchenko <dubchenkoegor@gmail.com>
2022-02-20 23:22:09 +0300

    test#1

---
+++ components/ChoiceProblem/ChoiceProblem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <n-button>
+      Test button
+    </n-button>
+  </div>
+</template>
+
+<script>
+import { NButton } from 'naive-ui'
+
+export default {
+  components: {
+    NButton
+  },
+
+  setup () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
